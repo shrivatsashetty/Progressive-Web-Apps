f8177c0c1ca933abcc8fb6c62baa48609ec8c3b4
shrivatsashetty <shrivatsa307@gmail.com>
2024-04-03 07:04:36 +0530

    for exams

--- serviceworker/public/sw.js
+++ serviceworker/public/sw.js
@@ -1,24 +1,29 @@
-const cacheName = "site_cache_v1";
+const cacheName = "site-cache-v1";
 
 const assetsToCache = [
+    /* this array contains the path of the resource files(html, css, js fiiles) to cache */
     "/",
     "index.html"
 ]
 
-self.addEventListener( 'install', (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntill(
         caches.open(cacheName) // creates a new cache obj (if not exists already), then passes it to a function to do something with it
-        .then((cache) => {  
-            return cache.addAll(assetsToCache); // all the assests like the html page, css styles, scripts are added to the cache object
-        })
+            .then((cache) => {
+                return cache.addAll(assetsToCache); // all the assests like the html page, css styles, scripts are added to the cache object
+            }).catch(err => {
+                console.log("Regsitration failed due to following error", err)
+            })
     );
 });
 
-
-self.addEventListener( 'activate', (event) => {
+// same process for activate event now
+self.addEventListener('activate', (event) => {
     event.waitUntill(
         caches.open(cacheName).then((cache) => {
             return cache.addAll(assetsToCache);
+        }).catch(err => {
+            console.log("Regsitration failed due to following error", err)
         })
     );
 });
